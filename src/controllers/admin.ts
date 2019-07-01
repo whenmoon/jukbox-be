10ab501f3688986e2bf6c22f9f509f4a2bb769cb
matthewtregg <tregearmatthew@gmail.com>
2019-07-01 12:42:01 +0200

    fix(src/services/token-stategy): prevented it from hitting done on both routes

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -49,8 +49,7 @@ export const setTransferPlayback = async (req: any, res: any) => {
     await transferPlayerPlayback(req.user.token, req.params.deviceid);
     res.status(204).send();
   } catch (e) {
-    console.error(e)
-    res.status(500).send(e);
+    res.status(e.error.error.status).send(e);
   }
 };
 
