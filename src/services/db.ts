5eb2ccd2661a3e508b0ca950f2a07c46a58f9c7f
matthewtregg <tregearmatthew@gmail.com>
2019-07-04 12:14:11 +0200

    environment variables for heroku

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,7 +2,12 @@ import { Pool } from 'pg';
 const host = process.env.DB_HOST;
 const database = process.env.DB_DATABASE;
 export const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
+//const connectionString = 'process.env.POSTGRES_CONNECTION_URL'
 
 const pool = new Pool({host, database, port});
-
+// const pool = new Pool({
+//   connectionString: connectionString,
+// })
 export default pool;
+
+
