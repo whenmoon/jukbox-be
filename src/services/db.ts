1505327a20c772259fe27901d3faa9188c668d29
matthewtregg <tregearmatthew@gmail.com>
2019-07-04 12:29:20 +0200

    changed database connection

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,13 +1,13 @@
 import { Pool } from 'pg';
-const host = process.env.DB_HOST;
-const database = process.env.DB_DATABASE;
-export const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
-//const connectionString = 'process.env.POSTGRES_CONNECTION_URL'
+// const host = process.env.DB_HOST;
+// const database = process.env.DB_DATABASE;
+// export const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432;
+const connectionString = 'process.env.DATABASE_URL'
 
-const pool = new Pool({host, database, port});
-// const pool = new Pool({
-//   connectionString: connectionString,
-// })
+//const pool = new Pool({host, database, port});
+const pool = new Pool({
+  connectionString: connectionString,
+})
 export default pool;
 
 
